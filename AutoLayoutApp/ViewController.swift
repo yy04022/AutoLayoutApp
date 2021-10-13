758e2f5e09dc0c2d3fc5a4abe92523fca7090fad
Jinyu Wang <ellie5201314@gmail.com>
2021-10-13 18:37:01 -0400

    Initial Commit

---
+++ AutoLayoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutApp
+//
+//  Created by Yu Jin on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
